2ea105667125f766dbc11ac59d4f0e5f9bfb2064
Trey Manuszak <manuszaktrey@gmail.com>
2024-12-07 13:13:11 -0500

    working on organization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+mod app;
+use app::run;
+
+#[tokio::main]
+async fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?; // Error reporting
+
+    run().await
 }
