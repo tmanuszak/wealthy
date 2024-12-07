6126efe1290e13c377d10171d0873528ee44c69c
Trey Manuszak <manuszaktrey@gmail.com>
2024-12-06 23:04:20 -0500

    Work on step 4

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.{html,rs}',
+    './src/**/*.{html,rs}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
